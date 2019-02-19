bd6b975f7c29cfdbdcb1307fbaef62bff1f0878b
Dace Howard <dace61@gmail.com>
2019-02-19 16:19:21 +1030

    Put close button back in plain sight

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $('#myInput').on('keyup', function() {
-	var searchVal = $(this).val();
-	var filterItems = $('[data-lightbox="photos"]');
+    var searchVal = $(this).val();
+    searchVal = searchVal.toLowerCase();
+    var filterItems = $('[data-lightbox="photos"]');
 
 	if ( searchVal != '' ) {
 		filterItems.addClass('hidden');
-		$('[data-lightbox="photos"][data-title*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+        $('[data-lightbox="photos"][data-title*="' + searchVal + '"]').removeClass('hidden');
 	} else {
 		filterItems.removeClass('hidden');
 	}
