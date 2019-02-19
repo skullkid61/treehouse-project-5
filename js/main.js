b1d30be8fbafa871bcf4f032651bda557716c31f
Dace Howard <dace61@gmail.com>
2019-02-19 16:32:18 +1030

    New search javascript
    Going through the old training videos to try a new approach

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $('#myInput').on('keyup', function() {
-    var searchVal = $(this).val();
-    searchVal = searchVal.toLowerCase();
+    var searchVal = $(this).val().toLowerCase();
     var filterItems = $('[data-lightbox="photos"]');
 
-	if ( searchVal != '' ) {
-		filterItems.addClass('hidden');
-        $('[data-lightbox="photos"][data-title*="' + searchVal + '"]').removeClass('hidden');
-	} else {
-		filterItems.removeClass('hidden');
-	}
+    for (var i = 0; i < filterItems.length; i++) {
+        var caption = $(filterItems[i]).attr("data-title").toLowerCase();
+        var thumbnail = $('.thumbnail')
+        if (caption.indexOf(searchVal) > -1) {
+            thumbnail[i].style.display = "";
+        } else {
+            thumbnail[i].style.display = "none";
+        }
+    }
 });
